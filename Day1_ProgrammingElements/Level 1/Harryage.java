0e7bcd5c6fa50435ef7ddec7cbf23f9ee75f552e
Ayushi0456 <ayushi.gla_cs24.gla.ac.in>
2025-08-22 18:28:57 +0530

    Added Level 1 and Level 2

---
+++ Day1_ProgrammingElements/Level 1/Harryage.java
@@ -0,0 +1,8 @@
+public class Harryage{
+    public static void main(String[]args){
+        int birthyear = 2000;
+        int currentyear = 2024;
+        int age = currentyear - birthyear;
+        System.out.println("Harry age in 2024 is " + age);
+    }
+}
---
+++ Day1_ProgrammingElements/Level 1/sammarks.java
@@ -0,0 +1,12 @@
+public class sammarks {
+    public static void main(String[]args){
+        int maths = 94;
+        int physics = 95;
+        int chemistry = 96;
+        int subjects = 3;
+        double average = (maths + physics + chemistry)/(double)subjects;
+        System.out.println("sam average marks in pcm is " + average);
+
+    }
+    
+}
