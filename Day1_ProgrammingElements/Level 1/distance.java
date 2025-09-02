a353d802bf7d1adf15d4d5dd8cf46e5885815c3d
Ayushi0456 <ayushi.gla_cs24@gla.ac.in>
2025-09-02 14:19:48 +0530

    Delete Day1_ProgrammingElements directory

--- Day1_ProgrammingElements/Level 1/distance.java
+++
@@ -1,8 +0,0 @@
-public class distance {
-    public static void main(String[]args){
-        double km = 10.8;
-        double miles = km*1.6;
-        System.out.println("10.8 kilometeres = " + miles + " miles" );
-    }
-    
-}
